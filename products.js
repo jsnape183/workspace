74e87776e93a66494bac2307c3a5d925952133d0
jsnape183 <jsnape183@gmail.com>
2025-06-04 14:37:49 +0000

    Change product to available

--- products.js
+++ products.js
@@ -2,7 +2,7 @@ const products = [
   {
     createdAt: "2025-06-03T12:26:30.852Z",
     productName: "Tasty Granite Pizza",
-    quantity: 46,
+    available: 46,
     unitPrice: 256.28,
     imageUrl: "https://loremflickr.com/218/1540?lock=3479084348145928",
     visible: false,
@@ -11,7 +11,7 @@ const products = [
   {
     createdAt: "2025-06-03T11:02:16.317Z",
     productName: "Modern Metal Cheese",
-    quantity: 18,
+    available: 18,
     unitPrice: 365.25,
     imageUrl: "https://loremflickr.com/1183/1575?lock=5377765413507357",
     visible: false,
@@ -20,7 +20,7 @@ const products = [
   {
     createdAt: "2025-06-03T12:11:34.921Z",
     productName: "Ergonomic Granite Shirt",
-    quantity: 30,
+    available: 30,
     unitPrice: 130.39,
     imageUrl: "https://loremflickr.com/2760/1055?lock=852974930570345",
     visible: false,
@@ -29,7 +29,7 @@ const products = [
   {
     createdAt: "2025-06-03T18:42:31.058Z",
     productName: "Recycled Marble Salad",
-    quantity: 14,
+    available: 14,
     unitPrice: 611.99,
     imageUrl: "https://loremflickr.com/2970/1343?lock=29658332895598",
     visible: false,
@@ -38,7 +38,7 @@ const products = [
   {
     createdAt: "2025-06-03T17:17:21.499Z",
     productName: "Modern Marble Soap",
-    quantity: 67,
+    available: 67,
     unitPrice: 891.08,
     imageUrl: "https://loremflickr.com/878/3765?lock=7542452976251965",
     visible: false,
@@ -47,7 +47,7 @@ const products = [
   {
     createdAt: "2025-06-04T00:05:50.175Z",
     productName: "Unbranded Granite Ball",
-    quantity: 45,
+    available: 45,
     unitPrice: 161.29,
     imageUrl: "https://loremflickr.com/2377/878?lock=2024810219282285",
     visible: false,
@@ -56,7 +56,7 @@ const products = [
   {
     createdAt: "2025-06-03T19:02:58.856Z",
     productName: "Fantastic Steel Bacon",
-    quantity: 89,
+    available: 89,
     unitPrice: 243.35,
     imageUrl: "https://loremflickr.com/2159/1866?lock=7629346834670900",
     visible: false,
@@ -65,7 +65,7 @@ const products = [
   {
     createdAt: "2025-06-03T16:51:07.129Z",
     productName: "Sleek Plastic Fish",
-    quantity: 61,
+    available: 61,
     unitPrice: 482.39,
     imageUrl: "https://loremflickr.com/3849/2565?lock=5849409955823852",
     visible: false,
@@ -74,7 +74,7 @@ const products = [
   {
     createdAt: "2025-06-03T17:44:52.263Z",
     productName: "Modern Cotton Soap",
-    quantity: 21,
+    available: 21,
     unitPrice: 283.85,
     imageUrl: "https://loremflickr.com/3167/3460?lock=7603001542077510",
     visible: false,
